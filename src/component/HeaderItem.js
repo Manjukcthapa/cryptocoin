857084206f03e9a7063296e3c18ac227ec6c9b81
Manju KC <Manju KC>
2019-05-05 15:19:41 -0400

    add images and add some style in headeritem file

--- src/component/HeaderItem.js
+++ src/component/HeaderItem.js
@@ -1,29 +1,33 @@
 import React from "react";
+import computer from "../images/computer.jpg"
+import secondImg from "../images/secondimg.jpg"
+import thirdImg from "../images/thirdimg.jpg"
+import forthImg from "../images/forthimg.jpg"
 
 const HeaderItem = (props) => {
     return(
         
      <div className="mainhead" Name={props.className}>
     <div className="textitem">
-      {/* <img src={}alt=""/> */}
+      <img className="images" src={computer}/>
       <p>Happy 6th Birthday! DATA Alliance, Block Explorers and more</p>
       <date>May 1, 2019</date>
       </div>
        
        <div className="textitem">
-      {/* <img src={computer.jpg} alt="name"/>  */}
+    <img className="images" src={secondImg}/>
       <p>2019: Year of the DEX</p>
       <date>April 4, 2019</date>
       </div>
 
       <div className="textitem">
-      <img src/>
+      <img className="images" src={thirdImg}/>
       <p>How blockchain will disrupt the communication industry</p>
       <date>March 22, 2019</date>
       </div>
       
       <div className="textitem">
-      <img/> 
+      <img  className="images" src={ forthImg}/> 
       <p>Life After Trading: Cryptocurrencies In Everyday Life</p>
       <date>March 15, 2019</date>
       </div>
